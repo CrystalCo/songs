17ab3e281793766bd5470bdc2c18cc3aa0515361
Crystal C <crystal.contr@gmail.com>
2019-01-10 00:13:48 -0600

    Added ui css classNames that make our app prettier using semantics grid layout.

--- src/components/SongList.js
+++ src/components/SongList.js
@@ -19,7 +19,7 @@ class SongList extends Component {
     }
 
     render() {
-        return <div> {this.renderList()} </div>
+        return <div className="ui divided list"> {this.renderList()} </div>
     }
 }
 
